d65d5551f5d4e324871e4d4e71888ea61215184c
kimkyungjoong <50091874+kimkyungjoong@users.noreply.github.com>
2024-05-02 18:47:52 +0900

    Update 0_automation_shortcut(홈화면 돌기).py

--- 0_automation_shortcut(홈화면 돌기).py
+++ 0_automation_shortcut(홈화면 돌기).py
@@ -690,5 +690,56 @@ except Exception as e:
 
 
 
+#######################################  다이닝 매거진  ##################################################
+
+try:
+    # 탭 링크 요소 찾기
+    tab_links = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".swiper-slide"))
+    )
+
+    max_retries = 1
+
+    # 링크를 클릭하여 해당 페이지에 접근
+    for i in range(len(tab_links)):
+        for attempt in range(max_retries):
+            try:
+                # 링크 다시 찾기
+                tab_links = WebDriverWait(driver, 10).until(
+                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".magazine-list-item"))
+                )
+                link = tab_links[i]
+                link_text = link.text
+                link.click()
+                print(f"{link_text} '다이닝 메거진' 페이지에 접근했습니다.")
+
+                # 페이지 로드를 기다림
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.TAG_NAME, "body"))
+                )
+
+                 # 스크롤 다운
+                time.sleep(1)
+                driver.execute_script("window.scrollBy(0, 10);")  # 수직으로 50 픽셀만큼 스크롤 다운
+                time.sleep(0.5)
+                driver.execute_script("window.scrollBy(0, 1000);")  # 수직으로 100 픽셀만큼 스크롤 다운
+                time.sleep(0.5)
+                driver.execute_script("window.scrollBy(0, 1500);")  # 수직으로 100 픽셀만큼 스크롤 다운
+                time.sleep(0.5)
+
+                # 뒤로 가기
+                driver.back()
+                time.sleep(1)
+                break  # 성공적으로 링크를 클릭했으면 다음 링크로 이동
+
+            except Exception as e:
+                print("링크를 찾고 있습니다.:", e)
+
+except Exception as e:
+    print(f"오류 발생: {e}")
+
+
+
+
 # 브라우저 종료
 driver.quit()
